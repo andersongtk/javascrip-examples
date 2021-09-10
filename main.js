52d73f580b100d4888455db3a6321ba11eac9120
andersongtk <anderson.gtk@gmail.com>
2021-09-10 19:19:19 +0300

    update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const repository = new WorkerRepository(window.localStorage);
 const popov = repository.save(new WorkerEntity("Andrey", "Popov", 36, "Manager"));
 const otroh = repository.save(new WorkerEntity("Ivan", "Otroh", 33, "Admin"));
 const motuz = repository.save(new WorkerEntity("Ira", "Motuz", 33, "Devops"));
-
+console.log(repository.all());
 setTimeout(function () {
     repository.delete(repository.findById(popov.getId()));
     repository.delete(repository.findById(otroh.getId()));
